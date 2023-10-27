3a2639dbc7650031ec64fde28b779ba1677ba5f0
あで <adelaide@fiberplane.com>
2023-10-27 15:35:51 +0200

    Add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect } from "react";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+
+import { client } from "./client";
+
+const SignIn = () => {
+  const handler = useCallback(() => {
+    client.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo:
+          "https://actualwitch.github.io/ask-autometrics-dashboard/success",
+      },
+    });
+  }, []);
+  return (
+    <div>
+      <button onClick={handler}>Sign in with Google</button>
+    </div>
+  );
+};
+
+const Success = () => {
+  useEffect(() => {
+    (async () => {
+      const user = await client.auth.getUser();
+      const session = await client.auth.getSession();
+      console.log({ user, session });
+
+      client
+        .from("docs.page")
+        .select()
+        .then((res) => {
+          console.log(res);
+        });
+    })();
+  }, []);
+  return <div>Success</div>;
+};
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      loader: () => redirect("/signin"),
+    },
+    {
+      path: "/signin",
+      Component: SignIn,
+    },
+    {
+      path: "/success",
+      Component: Success,
+    },
+  ],
+  { basename: "/ask-autometrics-dashboard" }
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+const ensureEnv = (name: string) => {
+  const actualName = `REACT_APP_${name}`;
+  if (!process.env[actualName]) {
+    throw new Error(`Missing env var: ${name}`);
+  }
+  return process.env[actualName];
+};
+
+const supabaseUrl = ensureEnv("SUPABASE_URL");
+const supabaseKey = ensureEnv("SUPABASE_KEY");
+
+export const client = createClient(supabaseUrl!, supabaseKey!);
